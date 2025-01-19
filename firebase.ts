dd69912dd80b5377e3d28d15b07aa394216954b7
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-01-19 20:15:37 +0300

    product

--- firebase.ts
+++ firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-// Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyBA_1biQnV4k1ZPKZz54oDCA8rynrO2arg",
-authDomain: "sudoperiba-abbdc.firebaseapp.com",
-projectId: "sudoperiba-abbdc",
-storageBucket: "sudoperiba-abbdc.firebasestorage.app",
-messagingSenderId: "680607767147",
-appId: "1:680607767147:web:236dd6998095cda572af2e",
-measurementId: "G-2D7M8371M6"
+  apiKey: "AIzaSyBA_1biQnV4k1ZPKZz54oDCA8rynrO2arg",
+  authDomain: "sudoperiba-abbdc.firebaseapp.com",
+  projectId: "sudoperiba-abbdc",
+  storageBucket: "sudoperiba-abbdc.firebasestorage.app",
+  messagingSenderId: "680607767147",
+  appId: "1:680607767147:web:236dd6998095cda572af2e",
+  measurementId: "G-2D7M8371M6"
 };
 
 // Initialize Firebase
